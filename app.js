b9a548343ca8ce6fbcdb4e076372c28bb02c5d1c
ZIIM <johntgause12@gmail.com>
2023-07-24 07:26:46 -0700

    more js for seattle section

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function randomNumber(){
 }
 
 // store the min/max hourly customers 
-const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
+// const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
 
 // average cookeis per customer
 function generateSales(){
@@ -21,8 +21,20 @@ function generateSales(){
 let seattleMin = 23;
 let seattleMax = 65;
 
-// const seattle = {
-//     minCustPerHour: seattleMin,
-//     maxCustPerHour: 
-
-// }
+const seattle = {
+    minCustPerHour: seattleMin,
+    maxCustPerHour: seattleMax,
+    avgCookies: 6.3,
+    getRandomCust: randomNumber,
+    hours:[
+        '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',
+    ],
+    cookieSales: [],
+    projectedCookieSales: function() {
+        for (let i = 0; i < this.hours.length; i++){
+            let cust = this.getRandomCust(); 
+            let cookiesPurchased = cust * this.avgCookies;
+            this.cookieSales.push(cookiesPurchased);
+        }
+    },
+};
