3314ecd9455087439291508a5c11af91aabc961a
ZIIM <johntgause12@gmail.com>
2023-07-24 06:10:12 -0700

    html js

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// store the min/max hourly customers 
+const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm']
+
+// average cookeis per customer
+function generateSales(){
+    for (let i = 0; i < this.hours.length; i++){
+        let customers = this.getRandomCust();
+        let cookiesPurchased = customers * this.avgCookies;
+        this.cookieSales.push(cookiesPurchased);
+    }
+}
+
+// generates random numbers between min and max of customer values
+function randomNumber(){
+    return Math.floor(
+        Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCust
+    );
+}
+
